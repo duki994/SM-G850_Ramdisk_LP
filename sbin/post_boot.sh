31bd1d8c35c108ccfde71d5a89b4ca33f7359dcc
duki994 <duki994@gmail.com>
2015-10-26 18:07:00 +0100

    Add more lines to post_boot

--- sbin/post_boot.sh
+++ sbin/post_boot.sh
@@ -6,3 +6,10 @@
 echo "500" > /sys/kernel/mm/uksm/sleep_millisecs
 # medium cpu gov
 echo "medium" > /sys/kernel/mm/uksm/cpu_governor
+########################
+
+
+### Set I/O deadline ###
+echo "deadline" > /sys/block/mmcblk0/queue/scheduler;
+echo "1024" > /sys/block/mmcblk0/bdi/read_ahead_kb
+echo "2" >  /sys/block/mmcblk0/queue/nomerges;
