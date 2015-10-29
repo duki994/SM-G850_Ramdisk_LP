d7a320ec319f7aef8b00ffc91ebcda97abd53f90
duki994 <duki994@gmail.com>
2015-10-29 01:35:05 +0100

    Enable new Synapse controls :)
    Needs cleanup and some fixes, but functions work ok :)

---
+++ res/synapse/json.gen.io
@@ -0,0 +1,38 @@
+#!/res/busybox sh
+
+export PATH=/res/asset:$PATH
+
+cat << CTAG
+{
+    name:I/O,
+    elements:[
+    
+	{ SPane:{
+		title:"I/O scheduler choose",
+        }},
+	{ SOptionList:{
+		title: "I/O sched",
+		description:"Set desired I/O sched",
+		default:deadline,
+		action:"generic /sys/block/mmcblk0/queue/scheduler",
+		values:[
+		     noop,
+		     deadline,
+		     cfq,
+		]
+	}},
+	{ SLiveLabel:{
+                  title:"I/O sched",
+                  description:"Current I/O sched.",
+                  refresh:5000,
+                  action:"cat /sys/block/mmcblk0/queue/scheduler"
+        }},
+	{ SSeekBar:{
+		title:"Read ahead value",
+		default:`cat /sys/block/mmcblk0/bdi/read_ahead_kb`,
+		action:"generic /sys/block/mmcblk0/bdi/read_ahead_kb",
+		unit:"kB", weight:256, min:0, step:1, max:10
+	}},
+    ]
+}
+CTAG
--- sbin/post_boot.sh
+++ sbin/post_boot.sh
@@ -1,15 +1,23 @@
-#!/system/bin/sh
 # duki994 - post_boot script for setting values after boot;
+#!/system/bin/sh
+BB=/res/busybox/
+
+### mount system and rootfs as rw
+$BB mount -t rootfs -o remount,rw
+$BB mount -o remount,rw /system
+
+#chmod everything in res dir
+$BB chmod -R 777 /res/*
 
 #### UKSM tuning #####
 # 500 ms scanning
-echo "500" > /sys/kernel/mm/uksm/sleep_millisecs
+$BB echo "500" > /sys/kernel/mm/uksm/sleep_millisecs
 # medium cpu gov
 echo "medium" > /sys/kernel/mm/uksm/cpu_governor
 ########################
 
 
 ### Set I/O deadline ###
-echo "deadline" > /sys/block/mmcblk0/queue/scheduler;
-echo "1024" > /sys/block/mmcblk0/bdi/read_ahead_kb
-echo "2" >  /sys/block/mmcblk0/queue/nomerges;
+$BB echo "deadline" > /sys/block/mmcblk0/queue/scheduler
+$BB echo "1024" > /sys/block/mmcblk0/bdi/read_ahead_kb
+$BB echo "2" >  /sys/block/mmcblk0/queue/nomerges
